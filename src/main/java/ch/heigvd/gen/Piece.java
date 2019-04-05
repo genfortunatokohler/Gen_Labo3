415b0fb0b9a6dd8649795892572dab659f710387
pete842 <pierrekohler94@gmail.com>
2019-04-05 10:19:47 +0200

    Add Piece with tests

--- src/main/java/ch/heigvd/gen/Piece.java
+++ src/main/java/ch/heigvd/gen/Piece.java
@@ -1,4 +1,13 @@
 package ch.heigvd.gen;
 
 public class Piece {
+    final private String name;
+
+    public Piece(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
