ee3733bf87f3981ffd17e37f38b9a80ef7adf827
Filipe Fortunato <filipe.fortunato@heig-vd.ch>
2019-04-05 11:40:13 +0200

    add board constructor with labels

--- src/main/java/ch/heigvd/gen/Board.java
+++ src/main/java/ch/heigvd/gen/Board.java
@@ -1,16 +1,26 @@
 package ch.heigvd.gen;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Board {
     private static final int squareCount = 40;
     private final ArrayList<Square> squares;
 
     public Board() {
+        this(IntStream.range(0, 40).mapToObj(Integer::toString).collect(Collectors.toList()));
+    }
+
+    public Board(List<String> labels) {
+        if (labels != null && labels.size() != squareCount)
+            throw new IllegalArgumentException();
+
         squares = new ArrayList<>(squareCount);
 
         for (int i = 0; i < squareCount; i++) {
-            squares.add(new Square(Integer.toString(i)));
+            squares.add(new Square(labels.get(i)));
         }
 
     }
