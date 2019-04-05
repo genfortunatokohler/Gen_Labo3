560c55ecf4fbbf5996afcecd464e328683a99d85
pete842 <pierrekohler94@gmail.com>
2019-04-05 12:04:41 +0200

    Add takeTurn in Player (+tests)

--- src/test/java/ch/heigvd/gen/PlayerTest.java
+++ src/test/java/ch/heigvd/gen/PlayerTest.java
@@ -1,18 +1,23 @@
 package ch.heigvd.gen;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
-    String name = "Toto";
-    Piece piece = new Piece("name");
-    Player player;
+    static String name = "Toto";
+    static Piece piece = new Piece("name");
+    static Die[] die = {new Die()};
+    static Board board;
+    static Player player;
 
-    @BeforeEach
-    void setUp() {
-        player = new Player(name, piece);
+    @BeforeAll
+    static void setUp() {
+        board = new Board();
+        player = new Player(board, die, name, piece);
     }
 
     @Test
@@ -23,5 +28,17 @@ class PlayerTest {
     @Test
     void getPiece() {
         assertEquals(piece, player.getPiece());
+    }
+    
+    @RepeatedTest(100)
+    void takeTurn() {
+        int oldLoc = board.getSquares().indexOf(player.getPiece().getLocation());
+
+        player.takeTurn();
+
+        int newLoc = board.getSquares().indexOf(player.getPiece().getLocation());
+
+        int shift = Math.floorMod((newLoc - oldLoc), board.getSquares().size());
+        assertTrue(shift > 0 && shift <= 12, Integer.toString(shift));
     }
 }
